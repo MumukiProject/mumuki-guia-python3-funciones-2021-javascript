195f55dab92d33be5f923339a42992f04506e24e
mayra@mumuki.org <mayra@mumuki.org>
2022-04-28 15:19:19 +0000

    Mumuki Export on 2022-04-28 15:19:18 +0000

--- 00018_Funciones que no funcionan/test..js
+++ 00018_Funciones que no funcionan/test..js
@@ -1,3 +1,19 @@
 it("mitad de 2 es 1", () => {
-  assert.equal(mitad(2, 1),1)
+  assert.equal(mitad(2),1)
+})
+
+it("mitad de 20 es 10", () => {
+  assert.equal(mitad(20),10)
+})
+
+it("mitad de 10 es 5", () => {
+  assert.equal(mitad(10),5)
+})
+
+it("suma de longitudes de 'hola' y 'mundo' es 9", () => {
+  assert.equal(sumaLongitudes('hola', 'mundo'),9)
+})
+
+it("suma de longitudes de 'llueva' y 'cafe' es 10", () => {
+  assert.equal(sumaLongitudes('llueva', 'cafe'),10)
 })
