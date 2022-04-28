eeead3617e7934eec5ecf4da5e5bacaf8bf63968
mayra@mumuki.org <mayra@mumuki.org>
2022-04-28 14:05:30 +0000

    Mumuki Export on 2022-04-28 14:05:30 +0000

--- 00017_Una definición, inifinitos usos/test..js
+++ 00017_Una definición, inifinitos usos/test..js
@@ -1,7 +1,7 @@
-class Test(unittest.TestCase):
+it("Rosario no es más largo que Bahia Blanca", () => {
+  assert(!esMasLargoQue('Rosario', 'Bahía Blanca'))
+})
 
-  def test_Rosario_no_es_mas_largo_que_Bahia_Blanca(self):
-    self.assertFalse(es_mas_largo_que("Rosario", "Bahía Blanca"))
-
-  def test_Vale_de_Uco_es_mas_largo_que_La_Punta(self):
-    self.assertTrue(es_mas_largo_que("Valle de Uco", "La Punta"))
+it("Valle de Uco no es más largo que Bahia Blanca", () => {
+  assert(esMasLargoQue('Valle de Uco', 'La Punta'))
+})
